26f8d7ec35db500dd3e4b0abe7fc906c6b8d1b02
djukes-byupathway <djukes@byupathway.edu>
2026-08-03 10:33:44 -0500

    edits from week 4 learning

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,6 +1,9 @@
 import db from './db.js'
 
 
+
+
+
 const getAllOrganizations = async () => {
     const query = `
         SELECT organization_id, name, description, contact_email, logo_filename
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,8 @@ import {
     showOrganizationDetailsPage,
     showNewOrganizationForm,
     processNewOrganizationForm,
-    organizationValidation
+    organizationValidation,
+    showEditOrganizationForm
 } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
@@ -20,10 +21,10 @@ router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/project/id', showProjectDetailsPage);
 router.get('/categories', showCategoriesPage);
-// Route for new organization page
 router.get('/new-organization', showNewOrganizationForm);
-// Route to handle new organization form submission
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+
+router.get('/edit-organization', showEditOrganizationForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
